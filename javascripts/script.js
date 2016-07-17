45c65b2ba7ef35fcd39e61590e58ae9508541e4a
tengyalun <alateng@gmail.com>
2016-07-17 19:58:06 +0800

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -41,7 +41,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for ( i=0 ; names.length ;i++ ) {
+for ( i=0 ; i< names.length ;i++ ) {
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
